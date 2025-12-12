bb60a3fd918d9184a7ae9939a3f0b0c344946c2b
Frieder Hannenheim <f.hannenheim@openthinclient.com>
2025-12-12 14:36:13 +0100

    Initial commit
    Co-authored-by: Marc Häfner <m.haefner@openthinclient.com>

---
+++ package-rootfs/opt/cisco/TeamsVDI/citrix-plugin-install.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ccf6b6b79e5428e03075bf9b168bb9cd32439fed78e0ad5b36eff77c89debd80
+size 3808
---
+++ package-rootfs/usr/bin/run_teamsvdi.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d06baac183e1029ca0d82221e124319df27fc73e7494864c91d8760294460bd
+size 756
